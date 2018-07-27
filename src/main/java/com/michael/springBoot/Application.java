27311a1dd978f57ebb8ea32fd9e507f9bbc2afbd
cuijian <jsflzhong@163.com>
2018-07-27 15:07:23 +0800

    first commit

--- src/main/java/com/michael/springBoot/Application.java
+++ src/main/java/com/michael/springBoot/Application.java
@@ -4,11 +4,21 @@ import com.michael.springBoot.dao.BaseMapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan(basePackages="com.michael.springBoot.dao",markerInterface = BaseMapper.class)
-@EnableScheduling
+//@EnableScheduling
+//@EnableEurekaServer
+@EnableDiscoveryClient //feign客户端
+@EnableFeignClients		//feign客户端
+@EnableHystrixDashboard	//hystrix仪表盘监控器
+@EnableCircuitBreaker	//hystrix监控器汇总工具s
 public class Application {
 
 	public static void main(String[] args) {
